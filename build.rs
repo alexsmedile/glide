5bc0f05cb88d3dd1fbf7aee1d0c577200acdb23b
Tyler Mandry <tmandry@gmail.com>
2026-06-19 23:32:03 -0700

    build: Only rerun build script if changed

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     println!(
         "cargo:rustc-link-search=framework={}",
         "/System/Library/PrivateFrameworks"
